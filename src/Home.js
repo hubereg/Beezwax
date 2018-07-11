0da9751e6f7f4cd9a8478582e47e85372aa60394
Mehmet Bajin <mehmet_b@beezwax.net>
2018-07-11 13:18:50 -0400

    feat: ids on home links

--- src/Home.js
+++ src/Home.js
@@ -6,16 +6,16 @@ const Home = () => (
     <h2>Home</h2>
     <ul>
       <li>
-        <Link to="/">Home</Link>
+        <Link to="/" id="link-to-home">Home</Link>
       </li>
       <li>
-        <Link to="/login">Login</Link>
+        <Link to="/login" id="link-to-login">Login</Link>
       </li>
       <li>
-        <Link to="/register">Register</Link>
+        <Link to="/register" id="link-to-register">Register</Link>
       </li>
       <li>
-        <Link to="/password">Forgot Password</Link>
+        <Link to="/password" id="link-to-forgot-password">Forgot Password</Link>
       </li>
     </ul>
   </div>
